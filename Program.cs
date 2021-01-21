75684a5fbd4e957c5170d156a5e5cb143333d07f
Soton <77579218+soton371@users.noreply.github.com>
2021-01-21 20:02:10 +0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Read two integer variables, calculate their sum, and print it.
+            int x = Convert.ToInt32(Console.ReadLine()), y= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x+y);
+        }
+    }
+}
